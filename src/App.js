c15226daac3dbe263ffaa4d873f625fd263e196a
plastikmind <mert.topkaya1@gmail.com>
2022-11-30 22:07:42 +0300

    logic update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Images from "./components/Images";
 
 function App() {
   const [fetchedData, setFetchedData] = useState([]);
-  const [search, setSearch] = useState("tree");
+  const [search, setSearch] = useState("");
   const [randomImage, setRandomImage] = useState("");
   const { results } = fetchedData;
 
@@ -22,24 +22,20 @@ function App() {
   const getData = async () => {
     const response = await axios.get(apiEndPoint);
     setFetchedData(response.data);
+    console.log("fuck", fetchedData);
   };
 
   useEffect(() => {
     getRandomImage();
     getData();
-  }, []);
+  }, [apiEndPoint]);
 
   return (
     <div className="flex flex-col items-center">
       <div className="flex relative mt-[10%] md:mt-[5%]">
-        {/* <img src={randomImage?.urls?.regular} className="relative -z-10" /> */}
+        <img src={randomImage?.urls?.regular} className="relative " />
 
-        <img
-          src="https://images.unsplash.com/photo-1669250494317-4251870813e9?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzODUzMDN8MHwxfHJhbmRvbXx8fHx8fHx8fDE2Njk4MDQ5MDI&ixlib=rb-4.0.3&q=80&w=1080"
-          className="relative "
-        />
-
-        <div className="absolute top-0 bottom-0 right-0 left-0 bg-black bg-opacity-25 items-center justify-center flex flex-col">
+        <div className="absolute top-0 bottom-0 right-0 left-0 bg-black bg-opacity-30 items-center justify-center flex flex-col">
           <p className="text-white text-center font-semibold text-4xl md:text-6xl">
             image search gallery
           </p>
